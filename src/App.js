d726ae55f66f66259b8da391821207b97c925b30
sahil <sahilsinghnayal@gmail.com>
2025-04-23 15:27:57 +0530

    TESTING

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Amplify }  from '@aws-amplify/core';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+// import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsconfig from './aws-exports';
 import React, { useState, useEffect } from 'react';
@@ -17,7 +17,7 @@ function App({ signOut, user }) {
   //   //signedIn();
   // }, []);
 
-  // const configureAuth = () => {
+  // const configureAuth = () => {-
   //   Amplify.configure(awsconfig);
   //   setIsConfigured(true);
   // };
@@ -32,4 +32,4 @@ function App({ signOut, user }) {
   );
 }
 
-export default withAuthenticator(App);
+export default App;
